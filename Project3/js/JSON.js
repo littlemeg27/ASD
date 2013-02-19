200991da2f8639a133f2926306330ffd95db1d84
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-02-18 23:26:12 -0500

    Made file for project 3 for uploading to cloudant

---
+++ Project3/js/JSON.js
@@ -0,0 +1,231 @@
+var json = 
+{
+"contact1":
+      {
+      "firstName":			["First Name:", "Brenna"],
+      "lastName": 			["Last Name:", "Pavlinchak"],
+      "phoneNumber":		["Phone Number:", "803-412-7815"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-06-25"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-06-25"],
+      "pools":				["What is your pool?", "Emmett Scott"],
+      "jobValue":			["Job:", "Lifeguard"], 
+       },
+    
+"contact2":
+      {
+      "firstName":			["First Name:", "Kenya"],
+      "lastName": 			["Last Name:", "Columbia"],
+      "phoneNumber":		["Phone Number:", "899-677-0618"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "Knightsbridge"],
+      },
+     
+"contact3":
+      {
+      "firstName":			["First Name:", "Ted"],
+      "lastName": 			["Last Name:", "Driskill"],
+      "phoneNumber":		["Phone Number:", "855-050-7072"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "Rock Hill Country Club"],
+      },
+      
+"contact4":
+      {	
+      "firstName":			["First Name:", "Julianne"],
+      "lastName": 			["Last Name:", "Depaolo"],
+      "phoneNumber":		["Phone Number:", "844-228-6795"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "Laurel Creek"],
+      },
+      
+"contact5":
+      {
+      "firstName":			["First Name:", "Hugh"],
+      "lastName": 			["Last Name:", "Falkner"],
+      "phoneNumber":		["Phone Number:", "899-775-6457"],
+      "email":				["Email:", "blogland@gmail.com"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-06-25"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-06-25"],
+      "pools":				["What is your pool?", "Rolling Hills"],
+      },
+
+"contact6":
+      {
+      "firstName":			["First Name:", "Christian"],
+      "lastName": 			["Last Name:", "Dooling"],
+      "phoneNumber":		["Phone Number:", "899-252-8700"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Shiland"],
+      },
+
+"contact7":
+      {
+      "firstName":			["First Name:", "Hugh"],
+      "lastName": 			["Last Name:", "Fail"],
+      "phoneNumber":		["Phone Number:", "822-515-6870"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-06-25"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-06-25"],
+      "pools":				["What is your pool?", "River Hills Country Club"],
+
+      },
+
+"contact8":
+      {
+      "firstName":			["First Name:", "Fernando"],
+      "lastName": 			["Last Name:", "Binder"],
+      "phoneNumber":		["Phone Number:", "899-097-7828"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Baxter"],
+      },
+
+"contact9":
+      {
+      "firstName":			["First Name:", "Saundra"],
+      "lastName": 			["Last Name:", "Woolum"],
+      "phoneNumber":		["Phone Number:", "833-041-4995"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-06-25"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-06-25"],
+      "pools":				["What is your pool?", "Emmett Scott"],
+      },
+
+"contact10":
+      {
+      "firstName":			["First Name:", "Fred"],
+      "lastName": 			["Last Name:", "Garrity"],
+      "phoneNumber":		["Phone Number:", "855-580-3811"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Oakwood"],
+      },
+
+"contact11":
+      {
+      "firstName":			["First Name:", "William"],
+      "lastName": 			["Last Name:", "Stoner"],
+      "phoneNumber":		["Phone Number:", "844-261-1312"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Northside"],
+      },
+
+"contact12":
+      {
+      "firstName":			["First Name:", "Melissa"],
+      "lastName": 			["Last Name:", "Milliner"],
+      "phoneNumber":		["Phone Number:", "844-192-9115"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Meadow Lakes 1"],
+      },
+
+
+"contact13":
+      {
+      "firstName":			["First Name:", "Lance"],
+      "lastName": 			["Last Name:", "Fanning"],
+      "phoneNumber":		["Phone Number:", "855-566-5627"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Meadow Lakes 2"],
+      },
+
+"contact14":
+      {
+      "firstName":			["First Name:", "Sharron"],
+      "lastName": 			["Last Name:", "Knight"],
+      "phoneNumber":		["Phone Number:", "855-075-9863"],
+      "cprDate":			["CPR Certification Date:", "2012-05-13"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Boyd Hill"],
+      },
+
+"contact15":
+      {
+      "firstName":			["First Name:", "Sharron"],
+      "lastName": 			["Last Name:", "Goto"],
+      "phoneNumber":		["Phone Number:", "899-252-6741"],
+      "email":				["Email:", "servercompass@gmail.com"],
+      "cprDate":			["CPR Certification Date:", "2012-05-04"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Northside"],
+      },
+
+"contact16":
+      {
+      "firstName":			["First Name:", "Lance"],
+      "lastName": 			["Last Name:", "Horace"],
+      "phoneNumber":		["Phone Number:", "822-337-5527"],
+      "cprDate":			["CPR Certification Date:", "2012-05-04"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Meadow Lakes 1"],
+      },
+      
+"contact17":
+      {
+      "firstName":			["First Name:", "Kathrine"],
+      "lastName": 			["Last Name:", "Barcus"],
+      "phoneNumber":		["Phone Number:", "899-946-6738"],
+      "cprDate":			["CPR Certification Date:", "2012-05-04"],
+      "firstAidDate":		["First Aid Certification Date:", "2011-12-10"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2011-12-10"],
+      "pools":				["What is your pool?", "Laurel Creek"],
+      },
+
+"contact18":
+      {
+      "firstName":			["First Name:", "Danny"],
+      "lastName": 			["Last Name:", "Chung"],
+      "phoneNumber":		["Phone Number:", "822-037-9010"],
+      "cprDate":			["CPR Certification Date:", "2012-05-04"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "Knightsbridge"],
+      },
+
+"contact19":
+      {
+      "firstName":			["First Name:", "Tyrone"],
+      "lastName": 			["Last Name:", "Boner"],
+      "phoneNumber":		["Phone Number:", "844-630-3721"],
+      "cprDate":			["CPR Certification Date:", "2012-05-04"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "Meadow Lakes 2"],
+      },
+      
+"contact20":
+      {
+      "firstName":			["First Name:", "Clinton"],
+      "lastName": 			["Last Name:", "Doshier"],
+      "phoneNumber":		["Phone Number:", "855-697-1252"],
+      "cprDate":			["CPR Certification Date:", "2012-05-31"],
+      "firstAidDate":		["First Aid Certification Date:", "2010-04-07"],
+      "lifeguardDate":		["Lifeguard Certification Date:", "2010-04-07"],
+      "pools":				["What is your pool?", "River Hills Country Club"],
+      },
+
+
+
+
+};
