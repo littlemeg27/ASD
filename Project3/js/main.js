6a998bd43945bb18734d88a1c9a2bcaded1c0032
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-02-20 00:23:27 -0500

    Got saveData to work, something weird going on with select list

--- Project3/js/main.js
+++ Project3/js/main.js
@@ -12,5 +12,35 @@
 	 
 	  $('#addItem').on('pageinit', function()
 	 {
-	     alert("The add item page is open");       
+	     alert("The add item page is open");
+	     
+			     $('#saveData').on('click', function()
+			     {
+				    var key;
+		            var id;
+		                
+		                if(!key)
+		                {
+		                    id = Math.floor(Math.random()*1000001);    
+		                }
+		                
+		                else
+		                {
+		                    id = key;
+		                }
+		                    
+		                    
+		            var item                  = {};
+		                item.firstName        =["First Name:", $("#firstName").val()];
+		                item.lastName         =["Last Name:", $("#lastName").val()];
+		                item.phoneNumber      =["Phone Number:", $("#phoneNumber").val()];
+		                item.pools            =["Pools:", $("#pools").val()];    
+		                                                
+		                localStorage.setItem(id, JSON.stringify(item)); 
+		                alert("Lifeguard Saved!");
+		                location.reload();
+		 
+			     });
+	            
 	 });   
+	 
